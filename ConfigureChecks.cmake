f17323826444729fce8a227ef8a170bb2af3a504
Tobias Koenig <tokoe@kde.org>
2010-10-08 14:06:35 +0000

    Merged revisions 1181945,1182205 via svnmerge from svn+ssh://tokoe@svn.kde.org/home/kde/trunk/KDE/kdepimlibs
    ........
      r1181945 | vkrause | 2010-10-02 21:42:07 +0200 (Sat, 02 Oct 2010) | 5 lines
      
      Introduce SharedValuePool, a little helper class that restores implicit
      sharing on objects that are received in great numbers but only with a
      small set of possible values. Use it for item mimetypes and flags,
      reducing their memory use from O(n) to O(1) for larger folder listings.
    ........
      r1182205 | vkrause | 2010-10-03 21:27:53 +0200 (Sun, 03 Oct 2010) | 4 lines
      
      Comment out the set version of SharedValuePool, currently unused and the
      template code to switch between the set and vector version fails to
      compile on Mac OS and Win CE at least.
    ........
    
    svn path=/branches/KDE/4.5/kdepimlibs/; revision=1183852

--- ConfigureChecks.cmake
+++
@@ -1,7 +0,0 @@
-include(CheckIncludeFiles)
-include(CheckLibraryExists)
-
-check_include_files(uuid/uuid.h HAVE_UUID_UUID_H)
-check_library_exists(uuid uuid_generate_random "" HAVE_UUID_LIBRARY)
-
-
