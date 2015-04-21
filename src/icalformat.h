KDE/kdepimlibs

ICalFormat: Introduce incidence parsing that ignores timezoneinformation.
This is orders of magnitudes faster when used in the akonadi serializer,
and uses a lot less memory (from 500mb to 100 for 10k events).

kcalcore/icalformat.h
@@ -100,6 +100,23 @@ public:
     Incidence::Ptr fromString(const QString &string);
 
     /**
+      Parses a string, returning the first iCal component as an Incidence, ignored timezone information.
+
+      This function is significantly faster than fromString by avoiding the overhead of parsing timezone information.
+      Timezones are instead solely interpreted by using system-timezones.
+
+      @param string is a utf8 QByteArray containing the data to be parsed.
+      @param tzlist is a collection of timezones used for the parsed date-times.
+      This collection may be empty or pre-populated. If it is empty, it is populated
+      automatically from the systemtimezones and thus acts as a cache. The tzlist may be 0
+      if the timezone should be read everytime from the system.
+
+      @return non-zero pointer if the parsing was successful; 0 otherwise.
+      @see fromString(const QString &), fromRawString()
+    */
+    Incidence::Ptr readIncidence(const QByteArray &string, ICalTimeZones *tzlist);
+
+    /**
       Parses a string and fills a RecurrenceRule object with the information.
 
       @param rule is a pointer to a RecurrenceRule object.

KDE/kcalendarcore

src/icalformat.h
@@ -101,6 +101,23 @@ public:
     Incidence::Ptr fromString(const QString &string);
 
     /**
+      Parses a bytearray, returning the first iCal component as an Incidence, ignoring timezone information.
+
+      This function is significantly faster than fromString by avoiding the overhead of parsing timezone information.
+      Timezones are instead solely interpreted by using system-timezones.
+
+      @param string is a utf8 QByteArray containing the data to be parsed.
+      @param tzlist is a collection of timezones used for the parsed date-times.
+      This collection may be empty or pre-populated. If it is empty, it is populated
+      automatically from the systemtimezones and thus acts as a cache. The tzlist may be 0
+      if the timezone should be read everytime from the system.
+
+      @return non-zero pointer if the parsing was successful; 0 otherwise.
+      @see fromString(const QString &), fromRawString()
+    */
+    Incidence::Ptr readIncidence(const QByteArray &string, ICalTimeZones *tzlist);
+
+    /**
       Parses a string and fills a RecurrenceRule object with the information.
 
       @param rule is a pointer to a RecurrenceRule object.