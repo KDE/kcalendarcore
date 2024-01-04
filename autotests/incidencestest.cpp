f17f9a0b290801d6a6e9a1f13203abd43af52001
Volker Krause <vkrause@kde.org>
2024-01-04 19:05:50 +0100

    Port incidencestest to QtTest and make it actually work
    This was failing previously, but since it indicated that only in debug
    output this went unnoticed.

--- autotests/incidencestest.cpp
+++ autotests/incidencestest.cpp
@@ -10,72 +10,43 @@
 #include "icalformat.h"
 #include "todo.h"
 
-#include <QCommandLineParser>
-#include <QCoreApplication>
 #include <QDebug>
-
-#include <stdlib.h>
+#include <QTest>
 
 using namespace KCalendarCore;
 
-int main(int argc, char **argv)
+class IncidencesTest : public QObject
 {
-    QCommandLineParser parser;
-    parser.addOption(QCommandLineOption(QStringList() << QStringLiteral("verbose"), QStringLiteral("Verbose output")));
+    Q_OBJECT
+private Q_SLOTS:
+    void testClone()
+    {
+        ICalFormat f;
 
-    QCoreApplication app(argc, argv);
-    QCoreApplication::setApplicationName(QStringLiteral("testincidence"));
-    QCoreApplication::setApplicationVersion(QStringLiteral("0.1"));
-    parser.process(app);
+        Event::Ptr event1 = Event::Ptr(new Event);
+        event1->setSummary(QStringLiteral("Test Event"));
+        event1->recurrence()->setDaily(2);
+        event1->recurrence()->setDuration(3);
+        event1->setSchedulingID(QStringLiteral("foo"));
+        QString eventString1 = f.toString(event1.staticCast<Incidence>());
 
-    const bool verbose = parser.isSet(QStringLiteral("verbose"));
+        Incidence::Ptr event2 = Incidence::Ptr(event1->clone());
+        QCOMPARE(event1->uid(), event2->uid());
+        QCOMPARE(event1->schedulingID(), event2->schedulingID());
 
-    ICalFormat f;
+        QString eventString2 = f.toString(event2.staticCast<Incidence>());
+        QCOMPARE(eventString1, eventString2);
 
-    Event::Ptr event1 = Event::Ptr(new Event);
-    event1->setSummary(QStringLiteral("Test Event"));
-    event1->recurrence()->setDaily(2);
-    event1->recurrence()->setDuration(3);
+        Todo::Ptr todo1 = Todo::Ptr(new Todo);
+        todo1->setSummary(QStringLiteral("Test todo"));
+        QString todoString1 = f.toString(todo1.staticCast<Incidence>());
 
-    QString eventString1 = f.toString(event1.staticCast<Incidence>());
-    if (verbose) {
-        qDebug() << "EVENT1 START:" << eventString1 << "EVENT1 END";
+        Incidence::Ptr todo2 = Incidence::Ptr(todo1->clone());
+        QString todoString2 = f.toString(todo2);
+        QCOMPARE(todoString1, todoString2);
     }
+};
 
-    event1->setSchedulingID(QStringLiteral("foo"));
-    Incidence::Ptr event2 = Incidence::Ptr(event1->clone());
+QTEST_APPLESS_MAIN(IncidencesTest)
 
-    Q_ASSERT(event1->uid() == event2->uid());
-    Q_ASSERT(event1->schedulingID() == event2->schedulingID());
-
-    QString eventString2 = f.toString(event2.staticCast<Incidence>());
-    if (verbose) {
-        qDebug() << "EVENT2 START:" << eventString2 << "EVENT2 END";
-    }
-
-    if (eventString1 != eventString2) {
-        qDebug() << "Clone Event FAILED.";
-    } else {
-        qDebug() << "Clone Event SUCCEEDED.";
-    }
-
-    Todo::Ptr todo1 = Todo::Ptr(new Todo);
-    todo1->setSummary(QStringLiteral("Test todo"));
-    QString todoString1 = f.toString(todo1.staticCast<Incidence>());
-    if (verbose) {
-        qDebug() << "todo1 START:" << todoString1 << "todo1 END";
-    }
-
-    Incidence::Ptr todo2 = Incidence::Ptr(todo1->clone());
-    QString todoString2 = f.toString(todo2);
-    if (verbose) {
-        qDebug() << "todo2 START:" << todoString2 << "todo2 END";
-    }
-
-    if (todoString1 != todoString2) {
-        qDebug() << "Clone Todo FAILED.";
-    } else {
-        qDebug() << "Clone Todo SUCCEEDED.";
-    }
-    return EXIT_SUCCESS;
-}
+#include "incidencestest.moc"
