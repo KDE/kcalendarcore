KDE/kcalendarcore

commit 6a18355546b6295ae830c0826fbdee7f2aaa10b0

General code cleanup
- more range-for loops
- use auto for iterator types

NO_CHANGELOG

src/calendar.cpp
@@ -36,7 +36,8 @@ extern "C" {
 #include <libical/icaltimezone.h>
 }
 
-#include <algorithm> // for std::remove()
+#include <algorithm>
+#include <set>
 
 using namespace KCalendarCore;
 
@@ -255,20 +256,20 @@ CalFilter *Calendar::filter() const
 
 QStringList Calendar::categories() const
 {
-    Incidence::List rawInc(rawIncidences());
-    QStringList cats;
+    const Incidence::List rawInc = rawIncidences();
+    QStringList uniqueCategories;
     QStringList thisCats;
     // @TODO: For now just iterate over all incidences. In the future,
     // the list of categories should be built when reading the file.
-    for (Incidence::List::ConstIterator i = rawInc.constBegin(); i != rawInc.constEnd(); ++i) {
-        thisCats = (*i)->categories();
-        for (QStringList::ConstIterator si = thisCats.constBegin(); si != thisCats.constEnd(); ++si) {
-            if (!cats.contains(*si)) {
-                cats.append(*si);
+    for (const Incidence::Ptr &inc : rawInc) {
+        thisCats = inc->categories();
+        for (const auto &cat : std::as_const(thisCats)) {
+            if (!uniqueCategories.contains(cat)) {
+                uniqueCategories.append(cat);
             }
         }
     }
-    return cats;
+    return uniqueCategories;
 }
 
 Incidence::List Calendar::incidences(const QDate &date) const
@@ -340,11 +341,11 @@ bool Calendar::updateNotebook(const QString &notebook, bool isVisible)
         return false;
     } else {
         d->mNotebooks.insert(notebook, isVisible);
-        const QList<Incidence::Ptr> incidences = d->mNotebookIncidences.values(notebook);
-        for (Incidence::Ptr incidence : incidences) {
-            QHash<Incidence::Ptr, bool>::Iterator it = d->mIncidenceVisibility.find(incidence);
-            if (it != d->mIncidenceVisibility.end()) {
-                *it = isVisible;
+        for (auto noteIt = d->mNotebookIncidences.cbegin(); noteIt != d->mNotebookIncidences.cend(); ++noteIt) {
+            const Incidence::Ptr &incidence = noteIt.value();
+            auto visibleIt = d->mIncidenceVisibility.find(incidence);
+            if (visibleIt != d->mIncidenceVisibility.end()) {
+                *visibleIt = isVisible;
             }
         }
         return true;
@@ -429,11 +430,10 @@ bool Calendar::setNotebook(const Incidence::Ptr &inc, const QString &notebook)
                 return false;
             }
             // Move all possible children also.
-            Incidence::List list = instances(inc);
-            Incidence::List::Iterator it;
-            for (it = list.begin(); it != list.end(); ++it) {
-                d->mNotebookIncidences.remove(old, *it);
-                d->mNotebookIncidences.insert(notebook, *it);
+            const Incidence::List list = instances(inc);
+            for (const auto &incidence : list) {
+                d->mNotebookIncidences.remove(old, incidence);
+                d->mNotebookIncidences.insert(notebook, incidence);
             }
             notifyIncidenceChanged(inc); // for removing from old notebook
             // don not remove from mUidToNotebook to keep deleted incidences
@@ -907,26 +907,22 @@ void Calendar::removeRelations(const Incidence::Ptr &incidence)
         // First, create a list of all keys in the mOrphans list which point
         // to the removed item
         relatedToUids << incidence->relatedTo();
-        for (QMultiHash<QString, Incidence::Ptr>::Iterator it = d->mOrphans.begin(); it != d->mOrphans.end(); ++it) {
+        for (auto it = d->mOrphans.cbegin(); it != d->mOrphans.cend(); ++it) {
             if (it.value()->uid() == uid) {
                 relatedToUids << it.key();
             }
         }
 
         // now go through all uids that have one entry that point to the incidence
-        for (QStringList::const_iterator uidit = relatedToUids.constBegin(); uidit != relatedToUids.constEnd(); ++uidit) {
-            Incidence::List tempList;
+        for (const auto &uid : std::as_const(relatedToUids)) {
             // Remove all to get access to the remaining entries
-            const Incidence::List l = values(d->mOrphans, *uidit);
-            d->mOrphans.remove(*uidit);
-            for (const Incidence::Ptr &i : l) {
-                if (i != incidence) {
-                    tempList.append(i);
-                }
-            }
+            Incidence::List lst = values(d->mOrphans, uid);
+            d->mOrphans.remove(uid);
+            lst.erase(std::remove(lst.begin(), lst.end(), incidence), lst.end());
+
             // Re-add those that point to a different orphan incidence
-            for (Incidence::List::Iterator incit = tempList.begin(); incit != tempList.end(); ++incit) {
-                d->mOrphans.insert(*uidit, *incit);
+            for (const auto &in : std::as_const(lst)) {
+                d->mOrphans.insert(uid, in);
             }
         }
     }

autotests/testfb.cpp
@@ -41,13 +41,13 @@ int main(int, char **)
     ICalFormat format;
     FreeBusy::Ptr fb = format.parseFreeBusy(fbString);
     qDebug() << fb->fullBusyPeriods().count() << " " << fb->dtStart().toString();
-    const FreeBusyPeriod::List l = fb->fullBusyPeriods();
-    for (FreeBusyPeriod::List::ConstIterator it = l.begin(); it != l.end(); ++it) {
-        qDebug() << (*it).start().toString() << " " << (*it).end().toString() << "+ " << (*it).summary() << ":" << (*it).location();
+    const FreeBusyPeriod::List lst = fb->fullBusyPeriods();
+    for (const auto &freebusy : lst) {
+        qDebug() << freebusy.start().toString() << " " << freebusy.end().toString() << "+ " << freebusy.summary() << ":" << freebusy.location();
     }
-    typedef QMap<QByteArray, QString> FooMap;
-    const FooMap props = fb->customProperties();
-    for (FooMap::ConstIterator it = props.begin(); it != props.end(); ++it) {
+
+    const QMap<QByteArray, QString> props = fb->customProperties();
+    for (auto it = props.cbegin(); it != props.cend(); ++it) {
         qDebug() << it.key() << ": " << it.value();
     }
 }

autotests/testrecurprevious.cpp
@@ -70,10 +70,9 @@ int main(int argc, char **argv)
         viewZone = QTimeZone(tz.toUtf8());
     }
     qDebug() << viewZone.id();
-    Incidence::List inc = cal->incidences();
+    const Incidence::List inc = cal->incidences();
 
-    for (Incidence::List::Iterator it = inc.begin(); it != inc.end(); ++it) {
-        Incidence::Ptr incidence = *it;
+    for (const Incidence::Ptr &incidence : inc) {
         qDebug() << "*+*+*+*+*+*+*+*+*+*";
         qDebug() << " ->" << incidence->summary() << "<-";
 

autotests/testrecurrencetype.cpp
@@ -66,10 +66,8 @@ int main(int argc, char **argv)
         viewZone = QTimeZone(tz.toUtf8());
     }
 
-    Incidence::List inc = cal->incidences();
-
-    for (Incidence::List::Iterator it = inc.begin(); it != inc.end(); ++it) {
-        Incidence::Ptr incidence = *it;
+    const Incidence::List inc = cal->incidences();
+    for (const Incidence::Ptr &incidence : inc) {
         qDebug() << "*+*+*+*+*+*+*+*+*+*";
         qDebug() << " ->" << incidence->summary() << "<-";
 

autotests/testrecurson.cpp
@@ -64,11 +64,8 @@ int main(int argc, char **argv)
     QString tz = cal->nonKDECustomProperty("X-LibKCal-Testsuite-OutTZ");
     const auto viewZone = tz.isEmpty() ? cal->timeZone() : QTimeZone(tz.toUtf8());
 
-    Incidence::List inc = cal->incidences();
-
-    for (Incidence::List::Iterator it = inc.begin(); it != inc.end(); ++it) {
-        Incidence::Ptr incidence = *it;
-
+    const Incidence::List inc = cal->incidences();
+    for (const Incidence::Ptr &incidence : inc) {
         //     qDebug() << " ->" << incidence->summary() << "<-";
 
         //     incidence->recurrence()->dump();

src/calfilter.cpp
@@ -149,27 +149,16 @@ bool CalFilter::filterIncidence(const Incidence::Ptr &incidence) const
         }
     }
 
-    if (d->mCriteria & ShowCategories) {
-        for (QStringList::ConstIterator it = d->mCategoryList.constBegin(); it != d->mCategoryList.constEnd(); ++it) {
-            QStringList incidenceCategories = incidence->categories();
-            for (QStringList::ConstIterator it2 = incidenceCategories.constBegin(); it2 != incidenceCategories.constEnd(); ++it2) {
-                if ((*it) == (*it2)) {
-                    return true;
-                }
-            }
+    const QStringList incidenceCategories = incidence->categories();
+    bool isFound = false;
+    for (const auto &category : std::as_const(d->mCategoryList)) {
+        if (incidenceCategories.contains(category)) {
+            isFound = true;
+            break;
         }
-        return false;
-    } else {
-        for (QStringList::ConstIterator it = d->mCategoryList.constBegin(); it != d->mCategoryList.constEnd(); ++it) {
-            QStringList incidenceCategories = incidence->categories();
-            for (QStringList::ConstIterator it2 = incidenceCategories.constBegin(); it2 != incidenceCategories.constEnd(); ++it2) {
-                if ((*it) == (*it2)) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
+
+    return d->mCriteria & ShowCategories ? isFound : !isFound;
 }
 
 void CalFilter::setName(const QString &name)

src/compat.cpp
@@ -350,10 +350,9 @@ void CompatOutlook9::fixAlarms(const Incidence::Ptr &incidence)
     if (!incidence) {
         return;
     }
-    Alarm::List alarms = incidence->alarms();
-    Alarm::List::Iterator end(alarms.end());
-    for (Alarm::List::Iterator it = alarms.begin(); it != end; ++it) {
-        Alarm::Ptr al = *it;
+
+    const Alarm::List alarms = incidence->alarms();
+    for (const Alarm::Ptr &al : alarms) {
         if (al && al->hasStartOffset()) {
             Duration offsetDuration = al->startOffset();
             int offs = offsetDuration.asSeconds();

src/customproperties.cpp
@@ -49,15 +49,15 @@ bool CustomProperties::Private::operator==(const CustomProperties::Private &othe
         // qCDebug(KCALCORE_LOG) << "Property count is different:" << mProperties << other.mProperties;
         return false;
     }
-    for (QMap<QByteArray, QString>::ConstIterator it = mProperties.begin(); it != mProperties.end(); ++it) {
-        QMap<QByteArray, QString>::ConstIterator itOther = other.mProperties.find(it.key());
-        if (itOther == other.mProperties.end() || itOther.value() != it.value()) {
+    for (auto it = mProperties.cbegin(); it != mProperties.cend(); ++it) {
+        auto itOther = other.mProperties.constFind(it.key());
+        if (itOther == other.mProperties.cend() || itOther.value() != it.value()) {
             return false;
         }
     }
-    for (QMap<QByteArray, QString>::ConstIterator it = mPropertyParameters.begin(); it != mPropertyParameters.end(); ++it) {
-        QMap<QByteArray, QString>::ConstIterator itOther = other.mPropertyParameters.find(it.key());
-        if (itOther == other.mPropertyParameters.end() || itOther.value() != it.value()) {
+    for (auto it = mPropertyParameters.cbegin(); it != mPropertyParameters.cend(); ++it) {
+        auto itOther = other.mPropertyParameters.constFind(it.key());
+        if (itOther == other.mPropertyParameters.cend() || itOther.value() != it.value()) {
             return false;
         }
     }
@@ -176,7 +176,7 @@ QString CustomProperties::nonKDECustomPropertyParameters(const QByteArray &name)
 void CustomProperties::setCustomProperties(const QMap<QByteArray, QString> &properties)
 {
     bool changed = false;
-    for (QMap<QByteArray, QString>::ConstIterator it = properties.begin(); it != properties.end(); ++it) {
+    for (auto it = properties.cbegin(); it != properties.cend(); ++it) {
         // Validate the property name and convert any null string to empty string
         if (checkName(it.key())) {
             if (d->isVolatileProperty(QLatin1String(it.key()))) {

commit e983e4df635fe907d5c9560bd6d7f50f005d13dd

Fix Calendar::updateNotebook event visibility updates
Broken by commit 6a18355546. Earlier this was using
QMultiHash::values(key) for updates but the commit made it loop over
all the events, thus when some notebook was updating visibility,
everything got updated accordingly.

src/calendar.cpp
@@ -338,7 +338,10 @@ bool Calendar::updateNotebook(const QString &notebook, bool isVisible)
         return false;
     } else {
         d->mNotebooks.insert(notebook, isVisible);
-        for (auto noteIt = d->mNotebookIncidences.cbegin(); noteIt != d->mNotebookIncidences.cend(); ++noteIt) {
+
+        for (auto  noteIt = d->mNotebookIncidences.find(notebook);
+             noteIt != d->mNotebookIncidences.end() && noteIt.key() == notebook;
+             ++noteIt) {
             const Incidence::Ptr &incidence = noteIt.value();
             auto visibleIt = d->mIncidenceVisibility.find(incidence);
             if (visibleIt != d->mIncidenceVisibility.end()) {