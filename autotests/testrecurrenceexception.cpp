KDE/kcalendarcore

commit bbe5752241864c2b566b5290968f448553d0fa62

Update recurrenceId when dtStart changes
When changing the dtStart of a recurring event,
the recurrence ids of exceptions don't match
anymore any of the occurrence dtStart. Exceptions
are thus ignored from the instance iterator, but
they are still listed as exceptions by the
Calendar::instances() method.

Correct this inconsistency by shifting the
recurrence ids of exceptions accordingly with
the change in dtStart of the recurring incidence.

autotests/testrecurrenceexception.cpp
@@ -36,4 +36,30 @@ void TestRecurrenceException::testCreateTodoException()
     // TODO dtCompleted
 }
 
+void TestRecurrenceException::testUpdateDtStart()
+{
+    const QDateTime dtstart(QDate(2025, 07, 22), QTime(13, 55, 0), QTimeZone::UTC);
+
+    KCalendarCore::Event::Ptr event(new KCalendarCore::Event);
+    event->setDtStart(dtstart);
+    event->setDtEnd(dtstart.addSecs(300));
+    event->recurrence()->setDaily(1);
+    event->recurrence()->setDuration(2);
+
+    KCalendarCore::MemoryCalendar::Ptr calendar(new KCalendarCore::MemoryCalendar(QTimeZone::utc()));
+    QVERIFY(calendar->addEvent(event));
+
+    KCalendarCore::Incidence::Ptr exception
+        = calendar->createException(event, dtstart.addDays(1));
+    QCOMPARE(exception->recurrenceId(), dtstart.addDays(1));
+    exception->setDtStart(exception->recurrenceId().addSecs(-3600));
+    QVERIFY(calendar->addIncidence(exception));
+
+    // Recurrence ids of exception should still match recurring
+    // event occurrences after a dtstart update.
+    event->setDtStart(dtstart.addSecs(300));
+    QVERIFY(event->recursAt(event->dtStart().addDays(1)));
+    QCOMPARE(exception->recurrenceId(), event->dtStart().addDays(1));
+}
+
 #include "moc_testrecurrenceexception.cpp"

autotests/testrecurrenceexception.h
@@ -14,6 +14,8 @@ class TestRecurrenceException : public QObject
     Q_OBJECT
 private Q_SLOTS:
     void testCreateTodoException();
+
+    void testUpdateDtStart();
 };
 
 #endif // TESTRECURRENCEEXCEPTION_H

src/memorycalendar.cpp
@@ -53,6 +53,7 @@ public:
     MemoryCalendar *q;
     CalFormat *mFormat; // calendar format
     QString mIncidenceBeingUpdated; //  Instance identifier of Incidence currently being updated
+    QDateTime mDtStartBeingUpdated; // dtStart field of the incidence being updated
     bool mUpdateLastModified; // Call setLastModified() on incidence modific ations
 
     /**
@@ -534,6 +535,9 @@ void MemoryCalendar::incidenceUpdate(const QString &uid, const QDateTime &recurr
         // Save it so we can detect changes to uid or recurringId.
         d->mIncidenceBeingUpdated = inc->instanceIdentifier();
 
+        // Save dtStart so we can detect a change and apply it to exceptions.
+        d->mDtStartBeingUpdated = inc->dtStart();
+
         const QDateTime dt = inc->dateTime(Incidence::RoleCalendarHashing);
         if (dt.isValid()) {
             d->mIncidencesForDate[inc->type()].remove(dt.toTimeZone(timeZone()).date(), inc);
@@ -567,6 +571,15 @@ void MemoryCalendar::incidenceUpdated(const QString &uid, const QDateTime &recur
         if (dt.isValid()) {
             d->mIncidencesForDate[inc->type()].insert(dt.toTimeZone(timeZone()).date(), inc);
         }
+
+        // When dstart changes, move recurrence ids of exception accordingly.
+        if (inc->recurs() && inc->dtStart() != d->mDtStartBeingUpdated) {
+            const Duration delta(d->mDtStartBeingUpdated, inc->dtStart());
+            for (Incidence::Ptr exception : instances(inc)) {
+                exception->setRecurrenceId(delta.end(exception->recurrenceId()));
+            }
+        }
+        d->mDtStartBeingUpdated == QDateTime();
 
         notifyIncidenceChanged(inc);
 

commit 3928e5981e5eb8d5b1e6ce6dcdc23483f45fcc7e

Fix compilation error with Qt5
Introduced by bbe57522 when cherry-picked from
the master branch, QDateTime::UTC is something
not working with Qt5.

autotests/testrecurrenceexception.cpp
@@ -38,7 +38,7 @@ void TestRecurrenceException::testCreateTodoException()
 
 void TestRecurrenceException::testUpdateDtStart()
 {
-    const QDateTime dtstart(QDate(2025, 07, 22), QTime(13, 55, 0), QTimeZone::UTC);
+    const QDateTime dtstart(QDate(2025, 07, 22), QTime(13, 55, 0), Qt::UTC);
 
     KCalendarCore::Event::Ptr event(new KCalendarCore::Event);
     event->setDtStart(dtstart);